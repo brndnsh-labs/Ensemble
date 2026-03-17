43190284e22dfe50b58a860c41326c972baf6004
Brandon Shea <brandon@debian-desktop.home.lan>
2026-03-17 16:53:59 -0400

    ui: add Piano Roots toggle to Chords settings

--- public/components/InstrumentSettings.jsx
+++ public/components/InstrumentSettings.jsx
@@ -91,6 +91,19 @@ export function InstrumentSettings({ module }) {
                     </SettingRow>
                 )}
 
+                {module === 'chords' && (
+                    <SettingRow label="Piano Roots" id="pianoRootsCheck">
+                        <Toggle
+                            id="pianoRootsCheck"
+                            checked={state.pianoRoots}
+                            onChange={(val) => {
+                                dispatch(ACTIONS.SET_PIANO_ROOTS, val);
+                                saveCurrentState();
+                            }}
+                        />
+                    </SettingRow>
+                )}
+
                 {module === 'harmony' && (
                     <SettingRow
                         label="Complexity"
