b44c45ca1b3d459ab06b594d42a74a43f488caa4
Brandon Shea <brandonshea@me.com>
2026-03-20 16:17:56 -0400

    chore: cleanup test files from previous branch

--- test_fix.js
+++
@@ -1,9 +0,0 @@
-import { binarySearchMap } from './public/utils.js';
-
-const stepMap = [
-    { start: 0, end: 16, chord: 'C' },
-    { start: 16, end: 32, chord: 'F' },
-];
-
-const match = binarySearchMap(stepMap, 16);
-console.log(match);
