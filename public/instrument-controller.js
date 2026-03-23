648e2534e4fd0a01631c00a6ebb7bad4c6d7b5f5
Brandon Shea <brandon@debian-desktop.home.lan>
2026-03-22 21:39:46 -0400

    refactor: enable proper code-splitting for drum presets
    - Removed redundant static import of DRUM_PRESETS in 'instrument-controller.js'.
    - This allows the bundler to lazy-load the 20KB preset data only when needed.

--- public/instrument-controller.js
+++ public/instrument-controller.js
@@ -1,4 +1,3 @@
-import { DRUM_PRESETS } from './data/drum-presets.js';
 import {
     killAllPianoNotes,
     killBassBus,
@@ -44,8 +43,9 @@ export function updateMeasures(val) {
 }
 
 /** @param {string} name */
-export function loadDrumPreset(name) {
+export async function loadDrumPreset(name) {
     const { groove, arranger } = getState();
+    const { DRUM_PRESETS } = await import('./data/drum-presets.js');
     let p = /** @type {any} */ (DRUM_PRESETS)[name];
     if (/** @type {any} */ (p)[arranger.timeSignature]) {
         p = { ...p, .../** @type {any} */ (p)[arranger.timeSignature] };
